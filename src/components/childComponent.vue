339d7126b707a5cc96fd3f47beb411ebcad49d81
Divyaa2000 <divya.bamrara@gmail.com>
2024-05-13 17:49:25 +0530

    first commit

---
+++ src/components/childComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Passing data from child to parent</h1>
+    <button @click="sendUser">Click me</button>
+</template>
+
+<script>
+export default {
+    name: 'childComponent',
+
+    data() {
+        return {
+            userName: "Hello"
+        }
+    },
+    props:{
+        getUser:Function
+
+    },
+
+    methods: {
+        sendUser() {
+            this.$emit('getUser', this.userName);
+        }
+    }
+}
+</script>
